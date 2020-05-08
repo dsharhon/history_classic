1201ae1ee1284010956bb3727b13e0514d5b87e3
Dylan Sharhon <dylan@sharhon.com>
2020-05-08 11:46:51 -0700

    Moar comments

--- src/popup.mjs
+++ src/popup.mjs
@@ -1,15 +1,15 @@
 const addItem = item => {
   const a = document.createElement('a')
-  a.title = item.title
+  a.title = item.title // tooltip
   a.href = item.url
   a.text = item.url
   a.target = '_blank'
   document.body.appendChild(a)
 }
 
-const text = ''
+const text = '' // wildcard search
 const history = maxResults => typeof browser === 'undefined'
-  ? new Promise(resolve => chrome.history.search({ text, maxResults }, resolve))
+  ? new Promise(resolve => chrome.history.search({ text, maxResults }, resolve)) // chrome
   : browser.history.search({ text, maxResults }) // firefox
 
 window.onload = async () => {
